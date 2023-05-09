1f44db13f196c50866c16322972535f96f9db9cd
Simon Günther <simon.guenther@sucurema.com>
2023-05-09 08:12:11 +0200

    ...

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -4,31 +4,30 @@ require("user.lsp.languages.python")
 require("user.lsp.languages.js-ts")
 
 lvim.format_on_save = true
-lvim.lsp.diagnostics.virtual_text = true
-
+vim.diagnostics.config({ virtual_text = true })
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "java",
-    "php",
-    "javascript",
-    "typescript",
-    "css",
-    "scss",
+	"java",
+	"php",
+	"javascript",
+	"typescript",
+	"css",
+	"scss",
 }
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls" })
 
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-    { command = "google_java_format", filetypes = { "java" } },
-    { command = "stylua",             filetypes = { "lua" } },
-    { command = "prettier",           filetypes = { "css", "scss", "rs", "ts", "js" } },
+	{ command = "google_java_format", filetypes = { "java" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "prettier", filetypes = { "css", "scss", "rs", "ts", "js", "py" } },
 })
 lvim.lsp.on_attach_callback = function(client, bufnr)
-    if client.server_capabilities.documentSymbolProvider then
-        local navic = require "nvim-navic"
-        navic.attach(client, bufnr)
-    end
+	if client.server_capabilities.documentSymbolProvider then
+		local navic = require("nvim-navic")
+		navic.attach(client, bufnr)
+	end
 end
 
 -- local linters = require "lvim.lsp.null-ls.linters"
